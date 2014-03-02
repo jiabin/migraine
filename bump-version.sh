907fbfeb59efd8937cd10656e50702925b6487ab
Eymen Gunay <eymen@egunay.com>
2014-03-02 22:08:08 +0100

    Bump version script updated

--- bump-version.sh
+++ bump-version.sh
@@ -53,7 +53,7 @@ git commit -m "Bump version ${TAG}"
 # Go back to master
 #
 git checkout master
+git push origin gh-pages
+git push origin ${TAG} 
 
-echo "New version created. Now you should run:"
-echo "git push origin gh-pages"
-echo "git push ${TAG}"
+echo "Success!"
